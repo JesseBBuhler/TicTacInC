4ce479c375255361c78a189e7c60fa703a36fc73
JesseBBuhler <66044271+JesseBBuhler@users.noreply.github.com>
2024-10-29 17:25:16 -0600

    fix winningMove check function

--- main.c
+++ main.c
@@ -8,9 +8,11 @@ bool moveTaken(int move, int board[BOARD_SIZE][BOARD_SIZE]);
 int getValidInput(int board[BOARD_SIZE][BOARD_SIZE]);
 int makeMove(int turn, int move, int board[BOARD_SIZE][BOARD_SIZE]);
 void clearLine();
-bool winningMove(int move);
+char winningMove(int move, int turn, int board[BOARD_SIZE][BOARD_SIZE]);
 int getCol(int move);
 int getRow(int move);
+char turnSymbol(int turn);
+int turnInt(int turn);
 
 int main() {
     int board[BOARD_SIZE][BOARD_SIZE];
@@ -30,26 +32,69 @@ int main() {
     while(winner == ' ' && turn < (BOARD_SIZE * BOARD_SIZE)) {
         printBoard(board);
         int move = getValidInput(board);
-        makeMove(((turn % 2) -1), move, board);
-        if(winningMove(move)) {
-            if(turn % 2) {
-                winner = 'O';
-            } else {
-                winner = 'X';
-            }
-
-        }
+        makeMove(turnInt(turn), move, board);
+        winner = winningMove(move, turn, board);
         turn++;
     }
     
     printBoard(board);
-    printf("%c won the game!", winner);
+    if(winner == ' ') {
+        printf("Game Over! It's a Tie!");
+    } else {
+        printf("%c won the game!", winner);
+    }
+    
     
     return 0;
 }
 
-bool winningMove(int move) {
-    return false;
+char turnSymbol(int turn) {
+    if(turn % 2) {
+        return 'O';
+    } else {
+        return 'X';
+    }
+}
+
+int turnInt(int turn) {
+    return (turn % 2)-1;
+}
+
+char winningMove(int move, int turn, int board[BOARD_SIZE][BOARD_SIZE]) {
+    bool colWin = true;
+    bool rowWin = true;
+    bool posWin = true;
+    bool negWin = true;
+    int col = getCol(move);
+    int row = getRow(move);
+    char symbol = turnSymbol(turn);
+
+    int checkCol = BOARD_SIZE - 1;
+    for (int i = 0; i < BOARD_SIZE; i++) {
+        if(board[row][i] != turnInt(turn)) {
+            rowWin = false;
+        }
+
+        if(board[i][col] != turnInt(turn)) {
+            colWin = false;
+        }
+
+        if(board[i][i] != turnInt(turn)) {
+            posWin = false;
+        }
+
+        if(board[i][checkCol] != turnInt(turn)) {
+            negWin = false;
+        }
+        checkCol--;
+    }
+    
+    if(colWin || rowWin || posWin || negWin) {
+        return symbol;
+    } else {
+        return ' ';
+    }
+    
 }
 
 int printBoard(int board[BOARD_SIZE][BOARD_SIZE]) {
